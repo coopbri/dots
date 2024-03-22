5c8fcafb348466d78a10d796ae327f90b1b78ef5
Brian Cooper <brian@brian-cooper.com>
2024-03-22 04:18:14 -0500

    feature(zsh): add 'sus' (system suspend) alias

--- .config/oh-my-zsh/alias.zsh
+++ .config/oh-my-zsh/alias.zsh
@@ -1,14 +1,15 @@
-# Development
+# TODO move all aliases here
+
+# development
 alias cg="~/Projects/clustergit/clustergit"
 alias spacemacs="emacs"
 
-# Package Management
+# package management
 alias up="yay -Syu"
 
 # VPN
 alias mc="mullvad connect"
 alias md="mullvad disconnect"
 
-# Django
-alias m="python manage.py"
-alias rundjango="python manage.py runserver"
+# system
+alias sus="systemctl suspend"
