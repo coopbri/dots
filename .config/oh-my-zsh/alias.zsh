71acf8e3718a9feeace60cdfcc7653335b30a919
Brian Cooper <brian@brian-cooper.com>
2024-12-24 14:19:32 -0600

    feature(zsh,alias): add 'zed', 'code', 'gstaf', 'bl', 'cilium' and 'talos' aliases

--- .config/oh-my-zsh/alias.zsh
+++ .config/oh-my-zsh/alias.zsh
@@ -1,8 +1,13 @@
-# TODO move all aliases here
+# TODO move all aliases here (K8s, Bun, etc.)
 
 # development
+alias zed="zeditor"
+alias code="zeditor"
 alias cg="~/Projects/clustergit/clustergit"
 alias spacemacs="emacs"
+
+# git
+alias gstaf="ls | xargs -i sh -c 'echo _______ {} ______ && cd {} && git status -s -uno && cd ..'"
 
 # package management
 alias up="yay -Syu"
@@ -14,3 +19,10 @@ alias md="mullvad disconnect"
 
 # system
 alias sus="systemctl suspend"
+
+# Bun
+alias bl="bun lint"
+
+# K8s
+alias cilium="cilium-cli"
+alias talos="talosctl"
